18cc2faa7a4e72b264378ecc7699ca5ab6dcce56
WaiHinKyaw1 <waihinkyaw2580@gmail.com>
2025-10-01 16:22:31 +0630

    change uat

--- src/components/AppSystemBarMimic.tsx
+++ src/components/AppSystemBarMimic.tsx
@@ -7,57 +7,58 @@ import WidgetsIcon from "@mui/icons-material/Widgets";
 import { useRouter, usePathname } from "next/navigation";
 
 export default function AppSystemBarMimic() {
-    const router = useRouter();
-    const pathname = usePathname();
-    const [tab, setTab] = useState(1);
-    const [lastVisited, setLastVisited] = useState<string | null>(null);
+  const router = useRouter();
+  const pathname = usePathname();
+  const [tab, setTab] = useState(1);
+  const [lastVisited, setLastVisited] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (pathname !== "/login") {
-            const previous = sessionStorage.getItem("lastVisited");
-            if (previous && previous !== pathname) setLastVisited(previous);
-            sessionStorage.setItem("lastVisited", pathname);
-        }
+  useEffect(() => {
+    if (pathname !== "/login") {
+      const previous = sessionStorage.getItem("lastVisited");
+      if (previous && previous !== pathname) setLastVisited(previous);
+      sessionStorage.setItem("lastVisited", pathname);
+    }
 
-        if (pathname === "/") setTab(1);
-        else setTab(0);
-    }, [pathname]);
+    if (pathname === "/") setTab(1);
+    else setTab(0);
+  }, [pathname]);
 
-    const handleNavigation = (index: number) => {
-        switch (index) {
-            case 0:
-                router.back();
-                break;
-            case 1:
-                router.push("/");
-                break;
-            case 2:
-                if (lastVisited && lastVisited !== pathname) router.push(lastVisited);
-                else router.push("/");
-                break;
-        }
-        setTab(index);
-    };
-    return (
-        <Paper
-            sx={{
-                position: "fixed",
-                bottom: 0,
-                left: 0,
-                right: 0,
-                zIndex: 999
-            }}
-            elevation={3}
-        >
-            <BottomNavigation
-                value={tab}
-                onChange={(_, newValue) => handleNavigation(newValue)}
-                showLabels
-            >
-                <BottomNavigationAction label="Back" icon={<ArrowBackIcon />} />
-                <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-                <BottomNavigationAction label="Recent" icon={<WidgetsIcon />} />
-            </BottomNavigation>
-        </Paper>
-    );
+  const handleNavigation = (index: number) => {
+    switch (index) {
+      case 0:
+        router.back();
+        break;
+      case 1:
+        router.push("/");
+        break;
+      case 2:
+        if (lastVisited && lastVisited !== pathname) router.push(lastVisited);
+        else router.push("/");
+        break;
+    }
+    setTab(index);
+  };
+  if (pathname === "/login") return null;
+  return (
+    <Paper
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 999,
+      }}
+      elevation={3}
+    >
+      <BottomNavigation
+        value={tab}
+        onChange={(_, newValue) => handleNavigation(newValue)}
+        showLabels
+      >
+        <BottomNavigationAction label="Back" icon={<ArrowBackIcon />} />
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Recent" icon={<WidgetsIcon />} />
+      </BottomNavigation>
+    </Paper>
+  );
 }
