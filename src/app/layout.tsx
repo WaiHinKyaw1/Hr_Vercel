4e07e3d1a5d9ad3e9e384ffe5fcd011e951bc3bd
WaiHinKyaw1 <waihinkyaw2580@gmail.com>
2025-11-10 14:55:03 +0630

    change image type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#1976d2" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icons/attendance192.png" sizes="192x192" />
-        <link rel="apple-touch-icon" href="/icons/attendance192.png" />
+        <link rel="icon" href="/icons/192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icons/192.png" />
       </head>
       <body className={`${raleway.variable}`}>
         <NextAuthProvider>
