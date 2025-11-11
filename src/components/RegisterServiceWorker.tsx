f737a030c74e4badbe7ac3d7033a3dd1884e6806
WaiHinKyaw1 <waihinkyaw2580@gmail.com>
2025-11-11 10:07:28 +0630

    change image and add offline file

--- src/components/RegisterServiceWorker.tsx
+++ src/components/RegisterServiceWorker.tsx
@@ -1,23 +1,19 @@
 "use client";
+
 import { useEffect } from "react";
 
 export default function RegisterServiceWorker() {
   useEffect(() => {
     if (typeof window === "undefined") return;
     if (process.env.NODE_ENV === "development") return;
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.getRegistrations().then((registrations) => {
-        for (const registration of registrations) {
-          registration.unregister().then((success) => {
-            console.log("Service Worker unregistered:", success);
-          });
-        }
-      });
 
+    if ("serviceWorker" in navigator) {
       window.addEventListener("load", async () => {
         try {
+          const registrations = await navigator.serviceWorker.getRegistrations();
+          for (const reg of registrations) await reg.unregister();
           const registration = await navigator.serviceWorker.register("/sw.js");
-          console.log("Service Worker registered with scope:", registration.scope);
+          console.log("Service Worker registered:", registration.scope);
         } catch (error) {
           console.error("Service Worker registration failed:", error);
         }
